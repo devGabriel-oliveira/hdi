69952a8ca7cab067c9340ff572cd20d7caf4af7c
devGabriel-oliveira <silvaogabriel10@gmail.com>
2026-02-08 12:46:25 -0300

    chore: base do projeto com users e settings

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    TIPO_USUARIO_CHOICES = (
+        ('PACIENTE', 'Paciente'),
+        ('PROFISSIONAL', 'Profissional da Saúde'),
+    )
+
+    tipo_usuario = models.CharField(
+        max_length=20,
+        choices=TIPO_USUARIO_CHOICES
+    )
